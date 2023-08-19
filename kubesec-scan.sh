98a6de1cf56a4de9dfd6f7c4b11f680b15d0a42b
Ashwin Radhakrishnan <ashwin.bittu@gmail.com>
2023-08-19 19:28:22 +1000

    Update kubesec-scan.sh

--- kubesec-scan.sh
+++ kubesec-scan.sh
@@ -4,6 +4,11 @@ scan_result=$(curl -sSX POST --data-binary @"trackerapp-k8s-manifests/base-manif
 scan_message=$(curl -sSX POST --data-binary @"trackerapp-k8s-manifests/base-manifests/UserMgmtWebApp-Deployment.yaml" https://v2.kubesec.io/scan | jq .[0].message -r )
 scan_score=$(curl -sSX POST --data-binary @"trackerapp-k8s-manifests/base-manifests/UserMgmtWebApp-Deployment.yaml" https://v2.kubesec.io/scan | jq .[0].score )
 
+echo "Results for UserMgmtWebApp-Deployment.yaml file"
+echo "#######################################"
+echo "Scan Result is $scan_result"
+echo "Scan Message is $scan_message"
+
 if [ "${scan_score}" -ge 5 ]; then
     echo "Score is $scan_score"
     echo "Kubesec Scan $scan_message"
@@ -16,6 +21,11 @@ fi
 scan_result=$(curl -sSX POST --data-binary @"trackerapp-k8s-manifests/base-manifests/mysql-deployment.yaml" https://v2.kubesec.io/scan)
 scan_message=$(curl -sSX POST --data-binary @"trackerapp-k8s-manifests/base-manifests/mysql-deployment.yaml" https://v2.kubesec.io/scan | jq .[0].message -r )
 scan_score=$(curl -sSX POST --data-binary @"trackerapp-k8s-manifests/base-manifests/mysql-deployment.yaml" https://v2.kubesec.io/scan | jq .[0].score )
+
+echo "Results for mysql-deployment.yaml file"
+echo "#######################################"
+echo "Scan Result is $scan_result"
+echo "Scan Message is $scan_message"
 
 if [ "${scan_score}" -ge 5 ]; then
     echo "Score is $scan_score"
