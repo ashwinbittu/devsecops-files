cc5d4d95a95ccbb828f9705dedffdfa48738563d
Ashwin Radhakrishnan <ashwin.bittu@gmail.com>
2023-08-19 17:47:39 +1000

    Update cis-aks-nodes.sh

--- cis-aks-nodes.sh
+++ cis-aks-nodes.sh
@@ -1,18 +1,25 @@
 #!/bin/bash
 #cis-aks.sh for workder nodes
-echo 'Starting K8S CIS Benchmark for Kubelet For Worker Nodes'          
+
+echo 'Starting K8S CIS Benchmark for Kubelet For Worker Nodes'
 sudo wget -qO /usr/local/bin/yq https://github.com/mikefarah/yq/releases/latest/download/yq_linux_amd64
 sudo chmod a+x /usr/local/bin/yq
 git clone https://github.com/aquasecurity/kube-bench.git
 
 kubectl delete job kube-bench
 
-for nodeName in $(kubectl get no -o=jsonpath='{.items[*].metadata.name}') do 
+for nodeName in $(kubectl get no -o=jsonpath='{.items[*].metadata.name}')
+do
+
   export nodeName=$nodeName
+  #cp kube-bench/job-node.yaml kubebench-aks-$nodeName.yaml
   cp kube-bench/job-aks.yaml kubebench-aks-$nodeName.yaml
   yq -i '.spec.template.spec.nodeName = env(nodeName)' kubebench-aks-$nodeName.yaml
   kubectl create -f kubebench-aks-$nodeName.yaml
   sleep 30s
+  #kubectl logs $(kubectl get pods --no-headers -o custom-columns=":metadata.name" | grep kube-bench-node) > $nodeName-kube-sec.logs
   kubectl logs $(kubectl get pods --no-headers -o custom-columns=":metadata.name" | grep kube-bench) > $nodeName-kube-sec.logs
   kubectl delete job kube-bench
+
 done
+
